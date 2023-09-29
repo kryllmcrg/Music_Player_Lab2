a028f8d73c03a6e24a1f457b07a38b410be1fe40
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-09-29 22:35:32 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/view', 'MainController::view');
+$routes->post('/createPlaylist', 'MainController::createPlaylist');
+$routes->post('/addsong', 'MainController::addsong');
